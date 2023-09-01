9384ca0eea6e82aee883d8597026bad6c771ad06
Samucaso <fernandessamuel10@gmail.com>
2023-08-31 21:13:45 -0300

    operadores logicos e variaveis

---
+++ estruturacondicionais/src/TesteOperadoresRelacionais1.java
@@ -0,0 +1,14 @@
+
+public class TesteOperadoresRelacionais1 {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		
+		System.out.println("a = " +a);
+		System.out.println("b = " + b);
+		System.out.println("a igual a b = " + (a==b));
+	}
+
+}
