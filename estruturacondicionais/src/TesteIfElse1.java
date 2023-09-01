62f0eea58de955181ceb81b94c461232461134c7
ADS 2023.2N <fernandessamuel10@gmail.com>
2023-08-31 21:35:26 -0300

    if else

--- estruturacondicionais/src/TesteIfElse1.java
+++ estruturacondicionais/src/TesteIfElse1.java
@@ -3,7 +3,19 @@ public class TesteIfElse1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 1;
+		int b = 2;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		if (a == b) {
+			System.out.println("a é igual a b");
+		} else {
+			System.out.println(" a é diferente de b");
+		}
+		
+		if (a != b) {
+			System.out.println("a é diferente do b");
+		}
 	}
 
 }
