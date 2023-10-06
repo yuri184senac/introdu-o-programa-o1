1bd0e5bfeb52e3ff30ca3e28c283fa5ea3bfe6e6
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2023-10-06 19:33:47 -0300

    jdk18

--- orientacaooop/src/main.java
+++ orientacaooop/src/main.java
@@ -4,10 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Conta conta1 = new Conta();
-		conta1.setNumero(21);
-		conta1.deposita(200);
-		System.out.println(conta1.getSaldo());
+		System.out.print("Classe main");;
 		
 	}
 
